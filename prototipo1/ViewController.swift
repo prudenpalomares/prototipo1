b428515cd632aabc3f2e34e81a77934fd1ea0702
Pruden <prucursos01@gmail.com>
2019-10-18 13:11:54 +0200

    Initial Commit

---
+++ prototipo1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prototipo1
+//
+//  Created by Mati on 18/10/2019.
+//  Copyright © 2019 Mati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
